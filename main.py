7e683ce76a678f8806535b2f6acaf8befca874b5
Peida Chen <18859993735@163.com>
2024-05-17 14:19:09 +0800

    commit-240517

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 def sayHello(name):
-  """ greet """
   print("Nice to meet you, %s!" %name)
 
+def sayBye(name):
+  print("Bye, %s!" %name)
 
 if __name__ == '__main__':
   sayHello("InnovateJoy")
+  sayBye("InnovateJoy")
